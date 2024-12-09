6c3f0d53f05ffefd4cc18d2a73b324208d8e94ba
Anish892003 <dhurianish74@gmail.com>
2024-12-09 18:30:13 +0530

    added navbar

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
+import Link from 'next/link'
+import { Phone, ShoppingBasket } from 'lucide-react'
+import { Button } from '../ui/button'
 
 const Header = () => {
     return (
         <header className='bg-white'>
-            <nav className='container py-5 px-80'>
+            <nav className='container py-5 flex items-center px-40 justify-between'>
 
                 <div className='flex items-center space-x-4'>
                     <svg
@@ -31,6 +34,25 @@ const Header = () => {
                         </SelectContent>
                     </Select>
 
+                </div>
+                <div className='flex items-center gap-4'>
+                    <ul className='flex items-center font-medium space-x-4'>
+                        <li><Link className='hover:text-primary' href={'/'}>Menu</Link></li>
+                        <li><Link className='hover:text-primary' href={'/'}>Orders</Link></li>
+                    </ul>
+                    <div className='relative'>
+                        <Link href='/cart'>
+                            <ShoppingBasket className='hover:text-primary' />
+                        </Link>
+                        <span className="absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white">
+                            3
+                        </span>
+                    </div>
+                    <div className="flex items-center ml-12">
+                        <Phone />
+                        <span>+91 9373 384 056</span>
+                    </div>
+                    <Button size={'sm'}>Logout</Button>
                 </div>
             </nav>
 
