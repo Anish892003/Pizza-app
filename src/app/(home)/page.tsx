e6e9f95224da844f2f6793654c57dd6e78a79828
Anish892003 <dhurianish74@gmail.com>
2024-12-09 19:03:51 +0530

    added category tabs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 
@@ -11,24 +12,38 @@ export default function Home() {
 
         <div className="container mx-auto flex items-center justify-between py-24 ">
           <div>
-            
-                <h1 className="text-7xl font-black font-sans leading-2">
-                  Super Delicious Pizza in <br />
-                  <span className="text-primary">Only 45 Minutes!</span>
-                </h1>
-                <p className="text-2xl mt-8 max-w-lg leading-snug">
-                  Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
-                </p>
-                <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
-                  Get your pizza now
-                </Button>
-              </div>
-              <div> 
-                <Image alt="pizza-main" src={'/pizza-main.png'} width={400} height={400} />
-                </div>
 
-            </div>
-          </section>
-        </>
-        );
+            <h1 className="text-7xl font-black font-sans leading-2">
+              Super Delicious Pizza in <br />
+              <span className="text-primary">Only 45 Minutes!</span>
+            </h1>
+            <p className="text-2xl mt-8 max-w-lg leading-snug">
+              Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
+            </p>
+            <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
+              Get your pizza now
+            </Button>
+          </div>
+          <div>
+            <Image alt="pizza-main" src={'/pizza-main.png'} width={400} height={400} />
+          </div>
+
+        </div>
+      </section>
+      <div className="container mx-auto py-12">
+        <Tabs defaultValue="pizza" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="pizza" className="text-md">Pizza</TabsTrigger>
+            <TabsTrigger value="beverages" className="text-md">Beverages</TabsTrigger>
+          </TabsList>
+          <TabsContent value="pizza">Pizza list</TabsContent>
+          <TabsContent value="beverages">Beverages list</TabsContent>
+        </Tabs>
+
+      </div>
+      <section>
+
+      </section>
+    </>
+  );
 }
