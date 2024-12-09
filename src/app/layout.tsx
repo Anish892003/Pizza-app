44e13662b07ed5ec05c0a5edc0c79ec10fb71902
Anish892003 <dhurianish74@gmail.com>
2024-12-09 18:05:47 +0530

    added logo and select box

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Header from "@/components/custom/header";
 
 const manrope = Manrope({ subsets: ['latin'], variable: "--font-manrope" });
 
@@ -22,8 +23,11 @@ export default function RootLayout({
           "min-h-screen bg-background font-manrope antialiased",
           manrope.variable
         )}>
+          <Header/>
+          
+          <main>{children}</main>
 
-        {children}
+      
       </body>
     </html>
   );
